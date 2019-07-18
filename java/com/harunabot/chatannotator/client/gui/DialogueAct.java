6aab459f4d004c02a6fa93b2d4c660886e4071a0
Haruna Ogawa <ogawa.h.ai@m.titech.ac.jp>
2019-07-18 16:22:19 +0900

    change DialogueAct

--- java/com/harunabot/chatannotator/client/gui/DialogueAct.java
+++ java/com/harunabot/chatannotator/client/gui/DialogueAct.java
@@ -5,12 +5,14 @@ import java.util.List;
 
 public enum DialogueAct
 {
-	QUESTION(0,"質問"),
-	SUGGEST (1,"提案"),
-	REQUEST (2,"要求"),
-	ACCEPT  (3, "承諾"),
-	DENY    (4, "拒否"),
-	CONVEY  (5, "伝達");
+	QUESTION(0, "質問"),
+	SUGGEST (1, "提案"),
+	GREETING(2, "挨拶"),
+	YES     (3, "はい"),
+	NO      (4, "いいえ"),
+	CONVEY  (5, "伝達"),
+	EXCLAMATION(6,"感嘆"),
+	CORRECT   (7, "訂正");
 
 	public static final List<DialogueAct> DIALOGUE_ACTS = new ArrayList<DialogueAct>();
 
@@ -47,7 +49,7 @@ public enum DialogueAct
 
 	public static List<DialogueAct> getList()
 	{
-		DialogueAct[] array = {QUESTION, SUGGEST, REQUEST, ACCEPT, DENY, CONVEY};
+		DialogueAct[] array = {QUESTION, SUGGEST, DialogueAct.GREETING,YES,DialogueAct.NO, CONVEY,EXCLAMATION, CORRECT};
 		List<DialogueAct> list = new ArrayList<DialogueAct>();
 		for(int i = 0; i < array.length; i++) {
 			list.add(array[i]);
